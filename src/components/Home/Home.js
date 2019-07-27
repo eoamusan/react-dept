7f9bfe7cbd21c921226dcb83944364ca26425cc8
Oluwatobi Amusan <tobi@kobo360.com>
2019-07-27 14:31:44 +0100

    In the beginning...

---
+++ src/components/Home/Home.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Home extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div className="height-auto full-width">
+                <div className="width-85-percent margin-auto top-margin-30">
+                    <div className="width-50-percent border-box float-left height-auto right-padding-10 bottom-margin-50">
+                        <div className="height-500 full-width black bottom-margin-20"></div>
+                        <div className="height-auto size-pointseven-rem gray-color">
+                            Bol.com
+                        </div>
+                        <div className="height-auto bottom-margin-10">
+                            A summer island in the Netherlands
+                        </div>
+                        <div className="height-auto size-pointeight-rem">
+                            VIEW CASE
+                        </div>
+                    </div>
+                    <div className="width-50-percent border-box float-left height-auto left-padding-10 bottom-margin-50">
+                        <div className="height-500 full-width black bottom-margin-20"></div>
+                        <div className="height-auto size-pointseven-rem gray-color">
+                            Bol.com
+                        </div>
+                        <div className="height-auto bottom-margin-10">
+                            A summer island in the Netherlands
+                        </div>
+                        <div className="height-auto size-pointeight-rem">
+                            VIEW CASE
+                        </div>
+                    </div>
+                    <div className="width-50-percent border-box float-left height-auto right-padding-10 bottom-margin-50">
+                        <div className="height-500 full-width black bottom-margin-20"></div>
+                        <div className="height-auto size-pointseven-rem gray-color">
+                            Bol.com
+                        </div>
+                        <div className="height-auto bottom-margin-10">
+                            A summer island in the Netherlands
+                        </div>
+                        <div className="height-auto size-pointeight-rem">
+                            VIEW CASE
+                        </div>
+                    </div>
+                    <div className="width-50-percent border-box float-left height-auto left-padding-10 bottom-margin-50">
+                        <div className="height-500 full-width black bottom-margin-20"></div>
+                        <div className="height-auto size-pointseven-rem gray-color">
+                            Bol.com
+                        </div>
+                        <div className="height-auto bottom-margin-10">
+                            A summer island in the Netherlands
+                        </div>
+                        <div className="height-auto size-pointeight-rem">
+                            VIEW CASE
+                        </div>
+                    </div>
+                    <div className="clear-both"></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header full-width">
+            <div className="full-width full-height black"></div>
+        </div>
+    )
+}
+
+export default Header
